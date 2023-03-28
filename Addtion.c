fd297cf0d5c14dd12964e32aa37f10aee0310d81
Sudarshan <123069294+Sudarshanshewale@users.noreply.github.com>
2023-03-28 13:55:39 +0530

    Add files via upload
    test

---
+++ Addtion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int no1, no2, sum;
+	printf("Enter first number");
+	scanf("%d",&no1);
+	printf("Enter second number");
+	scanf("%d",&no2);
+	sum= no1+no2;
+	printf("%d+%d=%d",no1, no2, sum);
+	
+	
+}
